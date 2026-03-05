646155d5cca09e73163dd348bf619736a3ab2486
Pankaj3402 <pankaj342002@gmail.com>
2026-03-05 07:36:29 +0530

    new changes added (#1)

--- CRUDUsingADO.Net/Controllers/CategoryController.cs
+++ CRUDUsingADO.Net/Controllers/CategoryController.cs
@@ -19,7 +19,6 @@ namespace CRUDUsingADO.Net.Controllers
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 con.Open();
-
                 string query = "SELECT * FROM dbo.Category";
 
                 using (SqlCommand cmd = new SqlCommand(query, con))
@@ -50,7 +49,6 @@ namespace CRUDUsingADO.Net.Controllers
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 con.Open();
-
                 string query = "SELECT * FROM dbo.Category WHERE Id=@Id";
 
                 using (SqlCommand cmd = new SqlCommand(query, con))
@@ -73,6 +71,40 @@ namespace CRUDUsingADO.Net.Controllers
         }
 
         // =========================
+        // CREATE (GET)
+        // =========================
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // =========================
+        // CREATE (POST)
+        // =========================
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = @"INSERT INTO dbo.Category (Name, OwnerName) 
+                                 VALUES (@Name, @OwnerName)";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", category.Name);
+                    cmd.Parameters.AddWithValue("@OwnerName", category.OwnerName);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // =========================
         // EDIT (GET)
         // =========================
         [HttpGet]
@@ -83,7 +115,6 @@ namespace CRUDUsingADO.Net.Controllers
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 con.Open();
-
                 string query = "SELECT * FROM dbo.Category WHERE Id=@Id";
 
                 using (SqlCommand cmd = new SqlCommand(query, con))
@@ -125,6 +156,60 @@ namespace CRUDUsingADO.Net.Controllers
                     cmd.Parameters.AddWithValue("@Name", category.Name);
                     cmd.Parameters.AddWithValue("@OwnerName", category.OwnerName);
 
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // =========================
+        // DELETE (GET) - SHOW CONFIRMATION
+        // =========================
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Category category = new Category();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "SELECT * FROM dbo.Category WHERE Id=@Id";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            category.Id = Convert.ToInt32(reader["Id"]);
+                            category.Name = reader["Name"].ToString();
+                            category.OwnerName = reader["OwnerName"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return View(category);   // ⚠ Only shows confirmation page
+        }
+        // =========================
+        // DELETE (POST) - CONFIRMED
+        // =========================
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "DELETE FROM dbo.Category WHERE Id=@Id";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
                     cmd.ExecuteNonQuery();
                 }
             }
